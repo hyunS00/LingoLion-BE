304785feed6bcd3804daf70ed3fa6a55c6917698
hyunS00 <optifun72@gmail.com>
2025-01-20 18:31:28 +0900

    feat: PATCH chats, DELETE chats 엔드포인트 생성

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateChatDto } from './dto/create-chat.dto';
 import { UpdateChatDto } from './dto/update-chat.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -24,11 +24,33 @@ export class ChatsService {
     return await this.chatRepository.findOne({ where: { id } });
   }
 
-  update(id: number, updateChatDto: UpdateChatDto) {
-    return `This action updates a #${id} chat`;
+  async update(id: number, updateChatDto: UpdateChatDto) {
+    const chat = await this.chatRepository.findOne({ where: { id } });
+
+    if (!chat) {
+      throw new NotFoundException();
+    }
+
+    await this.chatRepository.update(
+      { id },
+      {
+        ...updateChatDto,
+      },
+    );
+
+    const updatedChat = await this.chatRepository.findOne({ where: { id } });
+    return updatedChat;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} chat`;
+  async remove(id: number) {
+    const chat = await this.chatRepository.findOne({ where: { id } });
+
+    if (!chat) {
+      throw new NotFoundException();
+    }
+
+    await this.chatRepository.delete(id);
+
+    return id;
   }
 }
