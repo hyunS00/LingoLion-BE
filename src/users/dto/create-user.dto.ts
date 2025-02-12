5e4286d7f360432bc245c5750b2741061c944ab1
hyunS00 <optifun72@gmail.com>
2025-02-12 13:00:58 +0900

    Feat(user): createUserDto 비밀번호 유효성 검사 추가

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, Matches } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
@@ -6,6 +6,10 @@ export class CreateUserDto {
   email: string;
 
   @IsString()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      '비밀번호는 최소 8자 이상이며, 대문자, 소문자, 숫자 또는 특수 문자를 포함해야 합니다.',
+  })
   password: string;
 
   @IsString()
